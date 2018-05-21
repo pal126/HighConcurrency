117e155969915667adac2cbb8b544125f528d191
pal126 <wangkang@jskerun.com.cn>
2018-05-21 15:05:14 +0800

    atomic

---
+++ src/main/java/com/pal/concurrency/example/atomic/AtomicLongExample.java
@@ -0,0 +1,45 @@
+package com.pal.concurrency.example.atomic;
+
+import com.pal.concurrency.annoations.ThreadSafe;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 错误示例
+ */
+@ThreadSafe
+public class AtomicLongExample {
+
+    public static int clientTotal = 5000;
+    public static int threadTotal = 200;
+    public static AtomicLong count = new AtomicLong(0);
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        System.out.println("count:" + count.get());
+    }
+
+    private static void add() {
+        count.incrementAndGet();
+    }
+}
---
+++ src/main/java/com/pal/concurrency/example/atomic/LongAdderExample.java
@@ -0,0 +1,46 @@
+package com.pal.concurrency.example.atomic;
+
+import com.pal.concurrency.annoations.ThreadSafe;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * 正确示例
+ * 高并发下性能优于atomicInteger
+ */
+@ThreadSafe
+public class LongAdderExample {
+
+    public static int clientTotal = 5000;
+    public static int threadTotal = 200;
+    public static LongAdder count = new LongAdder();
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        System.out.println("count:" + count);
+    }
+
+    private static void add() {
+        count.increment();
+    }
+}
