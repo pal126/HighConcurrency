021850b9849ec979ca553631a5775056eb81f2c6
pal126 <wangkang@jskerun.com.cn>
2018-05-31 17:47:00 +0800

    atomic

---
+++ src/main/java/com/pal/concurrency/example/atomic/AtomicExampleBoolean.java
@@ -0,0 +1,45 @@
+package com.pal.concurrency.example.atomic;
+
+import com.pal.concurrency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Slf4j
+@ThreadSafe
+public class AtomicExampleBoolean {
+
+    private static AtomicBoolean isHappened = new AtomicBoolean(false);
+    public static int clientTotal = 5000;
+    public static int threadTotal = 200;
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    test();
+                    semaphore.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        log.info("isHappened:{}", isHappened.get());
+    }
+
+    private static void test() {
+        if (isHappened.compareAndSet(false, true)) {
+            log.info("success");
+        }
+    }
+}
